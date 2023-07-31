d10be17fa375d7f15b8b853214f5525a9c3b71b9
Attila Pataki <mcload6@yahoo.com>
2023-07-31 16:50:25 +0200

    modified for pvp

--- visualization.py
+++ visualization.py
@@ -1,21 +1,18 @@
 import pygame
+import numpy as np
 import sys
-import pvp
-import pvc    
+import time
+import pvsp
+import pvsc    
 
 def table_list_creator(board_size:int) -> list:
-    table_list = []
-    for i in range(board_size):
-        row_list = []
-        for j in range(board_size):
-            row_list.append(0)    
-        table_list.append(row_list)
+    table_list = np.full((board_size, board_size), ' ', dtype=str)
     return table_list
 
 
 def position_searcher(position: list, table_list: list) -> bool:
     (x, y) = position
-    return table_list[x][y] == 0
+    return table_list[x, y] == ' '
 
 
 def main():
@@ -36,6 +33,7 @@ def main():
 
     pvp = 1
     xoro = 1
+    win = 0
 
     while True:
 
@@ -50,31 +48,37 @@ def main():
                 pos_y = y // cell_size
                 position = [pos_x, pos_y]
                 if position_searcher(position, table_list):
-                    table_list[pos_x][pos_y] = xoro
+                    table_list[pos_x, pos_y] = xoro
                     if pvp == 0:
-                        if (win_line := pvp.winner(xoro, position, table_list)) is not False:
+                        if (win_line := pvsp.winner(xoro, position, table_list)) is not False:
                             win = 1
-                        if win == 0 and pvp.empty_space(table_list) is not False:
-                            [pos_x, pos_y] = pvc.next_step(table_list)
+                        if win == 0 and pvsp.empty_space(table_list) is not False:
+                            [pos_x, pos_y] = pvsc.next_step(table_list)
                             position = [pos_x, pos_y]
-                            table_list[pos_x][pos_y] = 2
+                            table_list[pos_x, pos_y] = 2
                             xoro = (xoro % 2) +1
-                    #if (win_line := winner(xoro, position, table_list)) is not False:
-                    #    win = 1
+                    if (win_line := pvsp.winner(str(xoro), table_list, cell_size)) is not False:
+                        win = 1
                     xoro = (xoro % 2) +1
 
         for i in range(board_size):
-            a = table_list[i]
             for j in range(board_size):
-                b = a[j]
-                if b == 1:
+                b = table_list[i, j]
+                if b == '1':
                     [x, y] = [i*cell_size, j*cell_size]
                     pygame.draw.line(screen, "blue", (x+10, y+10), (x+cell_size-10, y+cell_size-10), 5)
                     pygame.draw.line(screen, "blue", (x+10, y+cell_size-10), (x+cell_size-10, y+10), 5)
-                elif b == 2:
+                elif b == '2':
                     [x, y] = [i*cell_size, j*cell_size]
                     pygame.draw.circle(screen, "red", (x+cell_size//2, y+cell_size//2), cell_size//2-5, 5)
 
+        if win == 1:
+            [[x1, y1], [x2, y2]] = win_line
+            pygame.draw.line(screen, "red", (x1, y1), (x2, y2), 10)
+            pygame.display.flip()
+            time.sleep(2)
+            pygame.quit()
+            sys.exit()
 
         pygame.display.flip()
 
